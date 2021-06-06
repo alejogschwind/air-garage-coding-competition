e006fcd8d9accb09d74ea3e6b9800a2477fa0af0
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-06-06 17:20:08 -0300

    Fix api url prod.

--- src/api.js
+++ src/api.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const api_url = prcess.env.REACT_APP_DEV ? "http://127.0.0.1:8000/" : REACT_APP_API_URL;
+const api_url = process.env.REACT_APP_DEV ?
+  "http://127.0.0.1:8000/" :
+  REACT_APP_API_URL;
 
 const api = {
   getAllParkingLots: async ({ location, offset, sort_by }) =>
