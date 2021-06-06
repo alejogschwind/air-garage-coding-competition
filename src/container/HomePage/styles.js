6dc2a813f20de82c137c9256a1dac7d3da47e000
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-06-06 01:34:33 -0300

    Changed Welcome Background.

--- src/container/HomePage/styles.js
+++ src/container/HomePage/styles.js
@@ -75,7 +75,8 @@ export const WelcomeMessage = styled.div`
   max-width: 512px;
   padding: 2rem;
   margin: 2rem auto;
-  background: linear-gradient(90.19deg, #363636 0%, rgba(54, 54, 54, 0.9) 100%);
+  /* background: linear-gradient(90.19deg, #363636 0%, rgba(54, 54, 54, 0.9) 100%); */
+  background: linear-gradient(95.4deg, #FA824D 15.96%, rgba(250, 130, 77, 0.9) 100%);
   border-radius: 8px;
   color: #fcfcfc;
   font-size: 20px;
@@ -85,6 +86,8 @@ export const WelcomeMessage = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  box-shadow: 2px 4px 10px rgba(54, 54, 54, 0.1);
 
   & > strong, p {
     width: 100%;
