6727b80ad0b7d94eba0d0d22788f3245879d7188
Alejo Gschwind <alejogschwind.97@gmail.com>
2021-06-06 17:31:56 -0300

    Fix typo.

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const api_url = process.env.REACT_APP_DEV ?
   "http://127.0.0.1:8000/" :
-  REACT_APP_API_URL;
+  process.env.REACT_APP_API_URL;
 
 const api = {
   getAllParkingLots: async ({ location, offset, sort_by }) =>
